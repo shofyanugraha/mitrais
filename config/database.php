91dd35c451f3a9d949ec1275563cd6552bfe55e6
Rizky Nugraha <rizkyshofyan@gmail.com>
2019-07-30 21:35:27 +0700

    fix typho

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => $DATABASE_URL['host'],
-            'port' => $DATABASE_URL['port'];,
+            'port' => $DATABASE_URL['port'],
             'database' => ltrim($DATABASE_URL['path'], "/"),
             'username' => $DATABASE_URL['user'],
             'password' => $DATABASE_URL['pass'],
