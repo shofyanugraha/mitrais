196e32503b16ade3831257079c28832838706734
Rizky Nugraha <rizkyshofyan@gmail.com>
2019-07-30 22:41:24 +0700

    update meta

--- app/Transformers/Json.php
+++ app/Transformers/Json.php
@@ -49,22 +49,22 @@ class Json
             $message = __('message.error');
         }
 
-	    $result['message'] = $message;
-	    $result['status'] = false;
+	    $meta['message'] = $message;
+	    $meta['status'] = false;
         // dd();
         if ($error instanceof NotFoundHttpException) {    
-            $result['error']['message'] = $error->getMessage();
-            $result['error']['file'] = $error->getFile();
-            $result['error']['line'] = $error->getLine();
+            $meta['error']['message'] = $error->getMessage();
+            $meta['error']['file'] = $error->getFile();
+            $meta['error']['line'] = $error->getLine();
         } elseif ($error instanceof \Exception) {    
 
-            $result['error']['message'] = $error->getMessage();
-            $result['error']['file'] = $error->getFile();
-            $result['error']['line'] = $error->getLine();
+            $meta['error']['message'] = $error->getMessage();
+            $meta['error']['file'] = $error->getFile();
+            $meta['error']['line'] = $error->getLine();
         } elseif(is_array($error) && count($error) > 0) {
-    	   $result['error'] = $error; 
+    	   $meta['error'] = $error; 
         }
-	    return response()->json($result, $code);
+	    return response()->json(compact('meta'), $code);
     }
     
 }
